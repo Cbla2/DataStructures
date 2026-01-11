4807bfe67e83cf3ee017883a3e1039db5d15851e
Cbla2 <itscoroline@gmail.com>
2026-01-11 15:40:55 -0500

    Adding Int to linked list

--- Main.java
+++ Main.java
@@ -2,7 +2,15 @@
 public class Main {
   public static void main(String[] args) {
     
+    
+    LinkedList<Integer> nums = new LinkedList<>();
+    nums.add(1);
+    nums.add(2);
+    nums.add(3);
+    nums.print();
+
     LinkedList<String> list = new LinkedList<>();
+
     list.add("Apple");
     list.add("Turnip");
     list.add("Turkey");
